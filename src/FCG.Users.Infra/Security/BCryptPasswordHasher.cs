7b9be78ca2f094012e4e0941bbadf1b74c6ee7d2
João Melo <devjoaomelo@gmail.com>
2025-10-06 15:24:53 -0300

    Infra: Implementação do BCrypt

---
+++ src/FCG.Users.Infra/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using FCG.Users.Domain.Services;
+
+namespace FCG.Users.Infra.Security;
+
+public sealed class BCryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Password cannot be empty.", nameof(password));
+
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(hashedPassword))
+            return false;
+
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
